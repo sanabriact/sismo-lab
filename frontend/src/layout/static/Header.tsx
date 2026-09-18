6c8c03a079a129f38af29f99f1759e0f3bec8de6
Juan Felipe Sanabria <juan.sanabria64903@ucaldas.edu.co>
2026-09-18 11:14:07 -0500

    Adding sanabria branch.

--- frontend/src/layout/static/Header.tsx
+++ frontend/src/layout/static/Header.tsx
@@ -1,9 +1,6 @@
 const Header = () => {
     return(
         <header id="header-component">
-            <h1 className="header-title">
-                SismoLab
-            </h1>
         </header>
     );
 };
--- frontend/src/layout/static/Sidebar.tsx
+++ frontend/src/layout/static/Sidebar.tsx
@@ -1,18 +1,32 @@
 const Sidebar = () => {
     return (
         <div id="full-sidebar-test">
-            <h2 id="sidebar-title-test">
-                Sidebar de prueba
+            <h2 className="sidebar-title">
+                SismoLab
             </h2>
-            <h3>Elemento 1 de prueba</h3>
-            <h3>Elemento 2 de prueba</h3>
-            <h3>Elemento 3 de prueba</h3>
-            <h3>Elemento 4 de prueba</h3>
-            <h3>Elemento 5 de prueba</h3>
-            <h3>Elemento 6 de prueba</h3>
-            <h3>Elemento 7 de prueba</h3>
+            <div className="sidebar-list">
+                <div className="sidebar-option">
+                    <h4 id="modify-events">Dashboard</h4>
+                </div>
+                <h4>Elemento 2 de prueba</h4>
+                <h4>Elemento 3 de prueba</h4>
+                <h4>Elemento 4 de prueba</h4>
+                <h4>Elemento 5 de prueba</h4>
+                <h4>Elemento 6 de prueba</h4>
+                <h4>Elemento 7 de prueba</h4>
+                <h4>Elemento 8 de prueba</h4>
+                <h4>Elemento 9 de prueba</h4>
+            </div>
         </div>
     );
 };
+/* 
+    Añadir:
+    1. Botón con lista desplegable de opciones para eventos activos. Crear, consultar, corregir, marcar como revisado y eliminar individualmente.
+    2. 
 
+
+
+
+*/
 export default Sidebar;
